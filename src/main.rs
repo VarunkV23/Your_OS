011beff873d19de72e2aa089e9c050040ba03cc0
Varun Valecha <varunvalecha9@gmail.com>
2026-05-02 20:12:18 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![no_main]
 #![no_std]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop{}
 }
 
@@ -14,11 +18,25 @@ static HELLO: &[u8] = b"Hello World!";
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
 
-    vga_buffer::print_something();
-    // use core::fmt::Write;
-    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    // write!(vga_buffer::WRITER.lock(), ", some numbers: {} {} ", 42, 1.337).unwrap();
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
 
     loop{}
 }
 
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1,1);
+    println!("[ok]");
+}
