beb7d545b1a814a6955f927565d33fb2a958bb19
Varun Valecha <varunvalecha9@gmail.com>
2026-05-09 02:13:04 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
 #![no_main]
 #![no_std]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(your_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use core::panic::PanicInfo;
+use core::panic::{self, PanicInfo};
+use your_os::println;
 
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop{}
-}
-
-mod vga_buffer;
 
 static HELLO: &[u8] = b"Hello World!";
 #[unsafe(no_mangle)]
@@ -26,17 +20,15 @@ pub extern "C" fn _start() -> ! {
     loop{}
 }
 
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop{}
 }
 
-#[test_case]
-fn trivial_assertion() {
-    print!("trivial assertion... ");
-    assert_eq!(1,1);
-    println!("[ok]");
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    your_os::test_panic_handler(info)
 }
