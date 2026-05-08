48e51b29022047b5faafece0262c9aab2ce4e7be
Varun Valecha <varunvalecha9@gmail.com>
2026-05-09 02:13:24 +0530

    Update vga_buffer.rs

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -102,7 +102,7 @@ impl fmt::Write for Writer {
     }
 }
 
-// Only ONE WRITER — using lazy_static + Mutex
+// Only ONE WRITER, using lazy_static + Mutex
 lazy_static! {
     pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
@@ -126,4 +126,26 @@ macro_rules! println {
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
+}
+
+#[cfg(test_case)]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+
+#[test_case]
+fn test_println_many(){
+    for _ in 0..200  {
+        println!("test_println_many output")
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i,c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.char[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
 }
